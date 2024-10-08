e6e8b96ffb5012121a9ca7855718efa2e0c3f324
AkshayCdr <akshaychandra418@gmail.om>
2024-10-08 15:26:00 +0530

    added room-Id validation

--- app/routes/chatDashboard.tsx
+++ app/routes/chatDashboard.tsx
@@ -1,15 +1,63 @@
+import { ActionFunctionArgs, json } from "@remix-run/node";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
+import { Form, useActionData } from "@remix-run/react";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+    const formData = await request.formData();
+    const roomId = String(formData.get("roomId"));
+
+    console.log(roomId);
+    const err = isValidRoomId(roomId);
+
+    console.log(err);
+
+    if (err) {
+        return json(err);
+    }
+
+    return roomId;
+};
+
+type Errors = {
+    empty?: boolean;
+    invalid?: boolean;
+};
+
+const isValidRoomId = (roomId: string) => {
+    const errors: Errors = {};
+    if (!roomId) {
+        errors.empty = true;
+        return errors;
+    }
+
+    if (roomId.length < 3) {
+        errors.invalid = true;
+        return errors;
+    }
+    return null;
+};
+
 export default function chatDashboard() {
+    const actionData = useActionData<typeof action>() as Errors;
+    console.log(actionData);
     return (
         <main className="w-screen h-screen bg-green-900">
             <div className="flex flex-row items-center">
-                <Button variant={"blue"} className="w-56 h-14">
-                    New meeting
-                </Button>
+                <Form method="post">
+                    <Button variant={"blue"} className="w-56 h-14">
+                        New meeting
+                    </Button>
 
-                <Input type="email" placeholder="Email" />
-                <Button type="submit">Subscribe</Button>
+                    <Input name="roomId" type="text" placeholder="roomId" />
+                    {actionData?.empty && (
+                        <label className="text-red-700">Obj is empty</label>
+                    )}
+                    {actionData?.invalid && (
+                        <label className="text-red-700">Obj is invalid</label>
+                    )}
+                    <Button type="submit">Join</Button>
+                </Form>
             </div>
         </main>
     );
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
+import { createServer } from "http";
 
+import { Server } from "socket.io";
 // notice that the result of `remix vite:build` is "just a module"
 import * as build from "./build/server/index.js";
 
@@ -9,7 +11,17 @@ app.use(express.static("build/client"));
 
 // and your app is "just a request handler"
 app.all("*", createRequestHandler({ build }));
+const httpServer = createServer(app);
 
-app.listen(3000, () => {
-    console.log("App listening on http://localhost:3000");
+const io = new Server(httpServer, {});
+
+io.on("connection", (socket) => {
+    console.log("connected");
+    console.log(socket.id);
 });
+
+httpServer.listen(3000);
+
+// app.listen(3000, () => {
+//     console.log("App listening on http://localhost:3000");
+// });
