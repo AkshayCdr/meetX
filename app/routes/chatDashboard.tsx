7e66fde3554113da3656e43a1fbc6a93582c5203
AkshayCdr <akshaychandra418@gmail.om>
2024-10-08 13:53:05 +0530

    added dashboard for call

--- app/routes/chatDashboard.tsx
+++ app/routes/chatDashboard.tsx
@@ -1,5 +1,16 @@
 import { Button } from "../components/ui/button";
-
+import { Input } from "../components/ui/input";
 export default function chatDashboard() {
-    return <Button>click me</Button>;
+    return (
+        <main className="w-screen h-screen bg-green-900">
+            <div className="flex flex-row items-center">
+                <Button variant={"blue"} className="w-56 h-14">
+                    New meeting
+                </Button>
+
+                <Input type="email" placeholder="Email" />
+                <Button type="submit">Subscribe</Button>
+            </div>
+        </main>
+    );
 }
