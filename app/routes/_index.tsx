fde94a767d0e8812ac806049db955d92b0a92f32
AkshayCdr <akshaychandra418@gmail.om>
2024-10-17 12:57:24 +0530

    fixed login and singup eroor

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,15 @@
+import { Link } from "@remix-run/react";
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <div>
+            '
+            <ul>
+                <li>
+                    <Link to={"/login"}>Login</Link>
+                    <Link to={"/singup"}>Sign Up</Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
